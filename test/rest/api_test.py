5f272cfb09d971fa2140b51444c39bf76774569b
Ezequiel Jaramillo <sejr1996@gmail.com>
2023-05-17 22:59:35 -0500

    add integration tests with wrong parameters

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -2,6 +2,7 @@ import http.client
 import os
 import unittest
 from urllib.request import urlopen
+from urllib.error import HTTPError
 
 import pytest
 
@@ -23,6 +24,13 @@ class TestApi(unittest.TestCase):
         value = int(response.read().decode('utf-8'))
         self.assertEqual(value, 4)
 
+    def test_api_add_invalid_parameter(self):        
+        url = f"{BASE_URL}/calc/add/2/a2"
+
+        with self.assertRaises(HTTPError) as cm:
+            urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(cm.exception.code, 400)
+
     def test_api_substract(self):        
         url = f"{BASE_URL}/calc/substract/20/12"
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
@@ -30,6 +38,13 @@ class TestApi(unittest.TestCase):
         self.assertEqual(response.status, http.client.OK)
         value = int(response.read().decode('utf-8'))
         self.assertEqual(value, 8)
+
+    def test_api_substract_invalid_parameter(self):        
+        url = f"{BASE_URL}/calc/substract/20/d12"
+
+        with self.assertRaises(HTTPError) as cm:
+            urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(cm.exception.code, 400)
 
     def test_api_multiply(self):        
         url = f"{BASE_URL}/calc/multiply/5/12"
@@ -39,6 +54,13 @@ class TestApi(unittest.TestCase):
         value = int(response.read().decode('utf-8'))
         self.assertEqual(value, 60)
 
+    def test_api_multiply_invalid_parameter(self):        
+        url = f"{BASE_URL}/calc/multiply/u/12"
+
+        with self.assertRaises(HTTPError) as cm:
+            urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(cm.exception.code, 400)
+
     def test_api_divide(self):        
         url = f"{BASE_URL}/calc/divide/50/10"
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
@@ -47,6 +69,19 @@ class TestApi(unittest.TestCase):
         value = float(response.read().decode('utf-8'))
         self.assertEqual(value, 5.0)
 
+    def test_api_divide_by_zero(self):        
+        url = f"{BASE_URL}/calc/divide/50/0"
+
+        with self.assertRaises(HTTPError) as cm:
+            urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(cm.exception.code, 400)
+
+    def test_api_divide_invalid_parameter(self):        
+        url = f"{BASE_URL}/calc/divide/f/4"
+
+        with self.assertRaises(HTTPError) as cm:
+            urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(cm.exception.code, 400)
 
     def test_api_power(self):        
         url = f"{BASE_URL}/calc/power/2/5"
@@ -56,6 +91,12 @@ class TestApi(unittest.TestCase):
         value = int(response.read().decode('utf-8'))
         self.assertEqual(value, 32)
 
+    def test_api_power_invalid_parameter(self):        
+        url = f"{BASE_URL}/calc/power/u/12"
+
+        with self.assertRaises(HTTPError) as cm:
+            urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(cm.exception.code, 400)
 
     def test_api_sqrt(self):        
         url = f"{BASE_URL}/calc/sqrt/9"
@@ -65,6 +106,12 @@ class TestApi(unittest.TestCase):
         value = float(response.read().decode('utf-8'))
         self.assertEqual(value, 3.0)
 
+    def test_api_sqrt_invalid_parameter(self):        
+        url = f"{BASE_URL}/calc/sqrt/yy"
+        
+        with self.assertRaises(HTTPError) as cm:
+            urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(cm.exception.code, 400)
 
     def test_api_log10(self):        
         url = f"{BASE_URL}/calc/log10/100"
@@ -73,3 +120,10 @@ class TestApi(unittest.TestCase):
         self.assertEqual(response.status, http.client.OK)
         value = float(response.read().decode('utf-8'))
         self.assertEqual(value, 2.0)
+
+    def test_test_api_log10_invalid_parameter(self):        
+        url = f"{BASE_URL}/calc/log10/yy"
+        
+        with self.assertRaises(HTTPError) as cm:
+            urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(cm.exception.code, 400)
