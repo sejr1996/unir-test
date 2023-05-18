d0f4540d6c7c2adca573de1ec2d69ccff21cff60
Ezequiel Jaramillo <sejr1996@gmail.com>
2023-05-17 22:37:21 -0500

    add success integration tests

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -20,11 +20,56 @@ class TestApi(unittest.TestCase):
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
 
         self.assertEqual(response.status, http.client.OK)
-        self.assertEqual(response.read().decode('utf-8'), '4')
+        value = int(response.read().decode('utf-8'))
+        self.assertEqual(value, 4)
 
     def test_api_substract(self):        
         url = f"{BASE_URL}/calc/substract/20/12"
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
 
         self.assertEqual(response.status, http.client.OK)
-        self.assertEqual(response.read().decode('utf-8'), '8')
+        value = int(response.read().decode('utf-8'))
+        self.assertEqual(value, 8)
+
+    def test_api_multiply(self):        
+        url = f"{BASE_URL}/calc/multiply/5/12"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+
+        self.assertEqual(response.status, http.client.OK)
+        value = int(response.read().decode('utf-8'))
+        self.assertEqual(value, 60)
+
+    def test_api_divide(self):        
+        url = f"{BASE_URL}/calc/divide/50/10"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+
+        self.assertEqual(response.status, http.client.OK)
+        value = float(response.read().decode('utf-8'))
+        self.assertEqual(value, 5.0)
+
+
+    def test_api_power(self):        
+        url = f"{BASE_URL}/calc/power/2/5"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+
+        self.assertEqual(response.status, http.client.OK)
+        value = int(response.read().decode('utf-8'))
+        self.assertEqual(value, 32)
+
+
+    def test_api_sqrt(self):        
+        url = f"{BASE_URL}/calc/sqrt/9"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+
+        self.assertEqual(response.status, http.client.OK)
+        value = float(response.read().decode('utf-8'))
+        self.assertEqual(value, 3.0)
+
+
+    def test_api_log10(self):        
+        url = f"{BASE_URL}/calc/log10/100"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+
+        self.assertEqual(response.status, http.client.OK)
+        value = float(response.read().decode('utf-8'))
+        self.assertEqual(value, 2.0)
